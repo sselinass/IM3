97d635f7a2241f4d27a0d3b620603e7323550d51
Selina Schöpfer <selina.schoepfer@stud.fhgr.ch>
2025-10-06 11:12:47 +0200

    Update extract.php

--- php/extract.php
+++ php/extract.php
@@ -16,19 +16,23 @@
 
 function fetchWeatherData()
 {
-    $url = "https://api.open-meteo.com/v1/forecast?latitude=46.9481,46.8499,47.3667&longitude=7.4474,9.5329,8.55&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,rain,showers,snowfall,cloud_cover&temperature_unit=fahrenheit&timezone=auto&forecast_days=1";
+    $url = "https://api.citybik.es/v2/networks/publibike-bern";
 
     // Initialisiert eine cURL-Sitzung
-
+    $ch = curl_init($url);
 
     // Setzt Optionen
-
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
     // Führt die cURL-Sitzung aus und erhält den Inhalt
-
+    $response = curl_exec($ch);
 
     // Schließt die cURL-Sitzung
+    echo $response;
+    echo "<br><br>";
+    print_r($response);
 
+    curl_close($ch);
 
     // Dekodiert die JSON-Antwort und gibt Daten zurück
 
